234c76de4ad5c67bcf945d116d3d97eb68c37df6
alazhar98 <92651154+alazhar98@users.noreply.github.com>
2024-09-23 11:11:19 +0400

    Add files via upload

--- javaExamplesCode/FindRepeatedCharacter.java
+++ javaExamplesCode/FindRepeatedCharacter.java
@@ -1,5 +1,3 @@
-import java.util.HashSet;
-
 public class FindRepeatedCharacter {
     public static void main(String[] args) {
         String word = "swiss";
@@ -7,25 +5,26 @@ public class FindRepeatedCharacter {
 
         if (result != null) {
             System.out.println("First repeated character: '" + result + "'");
-
         } else {
             System.out.println("No repeated character found.");
         }
-
-
     }
 
     public static Character repeatedChar(String word) {
-        HashSet<Character> chars = new HashSet<>();
+
+        boolean[] seen = new boolean[26]; // Array to track characters with number of charecters
 
         for (char ch : word.toCharArray()) {
-            if (chars.contains(ch)) {
+            int index = ch - 'a'; // Calculate index [index of letter a is 0 and so on ]
+            // If the character has seen, return it
+            if (seen[index]) {
                 return ch;
             }
-            chars.add(ch);
+            // Mark this character as seen
+            seen[index] = true;
         }
+
+        // If no character is repeated return null
         return null;
     }
 }
-
-
